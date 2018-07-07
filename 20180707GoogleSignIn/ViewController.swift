d704e9d41410048dcdaa783c8d59603551bd36f0
m-sakai <re0026ir@gmail.com>
2018-07-07 13:33:05 +0900

    Initial Commit

---
+++ 20180707GoogleSignIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180707GoogleSignIn
+//
+//  Created by 酒井理也 on 2018/07/07.
+//  Copyright © 2018年 酒井理也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
