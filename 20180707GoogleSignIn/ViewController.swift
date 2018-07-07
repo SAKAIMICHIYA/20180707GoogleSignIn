f3691629e30824cb753afbb5c45e9a6f038a76fc
m-sakai <re0026ir@gmail.com>
2018-07-07 15:22:33 +0900

    20180707 GoogleSignIn ver0.1

--- 20180707GoogleSignIn/ViewController.swift
+++ 20180707GoogleSignIn/ViewController.swift
@@ -7,19 +7,90 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+// Match the ObjC symbol name inside Main.storyboard.
+@objc(ViewController)
+// [START viewcontroller_interfaces]
+class ViewController: UIViewController, GIDSignInUIDelegate {
+    // [END viewcontroller_interfaces]
+    // [START viewcontroller_vars]
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var statusText: UILabel!
+    // [END viewcontroller_vars]
+    // [START viewdidload]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        // Uncomment to automatically sign in the user.
+        //GIDSignIn.sharedInstance().signInSilently()
+        // TODO(developer) Configure the sign-in button look/feel
+        // [START_EXCLUDE]
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ViewController.receiveToggleAuthUINotification(_:)),
+                                               name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
+                                               object: nil)
+        
+        statusText.text = "Initialized Swift app..."
+        toggleAuthUI()
+        // [END_EXCLUDE]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // [END viewdidload]
+    // [START signout_tapped]
+    @IBAction func didTapSignOut(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().signOut()
+        // [START_EXCLUDE silent]
+        statusText.text = "Signed out."
+        toggleAuthUI()
+        // [END_EXCLUDE]
     }
-
-
+    // [END signout_tapped]
+    // [START disconnect_tapped]
+    @IBAction func didTapDisconnect(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().disconnect()
+        // [START_EXCLUDE silent]
+        statusText.text = "Disconnecting."
+        // [END_EXCLUDE]
+    }
+    // [END disconnect_tapped]
+    // [START toggle_auth]
+    func toggleAuthUI() {
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+            // Signed in
+            signInButton.isHidden = true
+            signOutButton.isHidden = false
+            disconnectButton.isHidden = false
+        } else {
+            signInButton.isHidden = false
+            signOutButton.isHidden = true
+            disconnectButton.isHidden = true
+            statusText.text = "Google Sign in\niOS Demo"
+        }
+    }
+    // [END toggle_auth]
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
+                                                  object: nil)
+    }
+    
+    @objc func receiveToggleAuthUINotification(_ notification: NSNotification) {
+        if notification.name.rawValue == "ToggleAuthUINotification" {
+            self.toggleAuthUI()
+            if notification.userInfo != nil {
+                guard let userInfo = notification.userInfo as? [String:String] else { return }
+                self.statusText.text = userInfo["statusText"]!
+            }
+        }
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-20180707GoogleSignIn/Pods-20180707GoogleSignIn-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_20180707GoogleSignIn : NSObject
+@end
+@implementation PodsDummy_Pods_20180707GoogleSignIn
+@end
